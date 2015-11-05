327da247020bc62c006846aad41149fbccf92101
Jay <jatacid@gmail.com>
2015-11-06 06:21:37 +1000

    Updates

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+~Current Version:1.0.4~
+
 # bb-template-as-header
 A Beaver Builder plugin which allows you to select any template (global row?) and inserts it before the BB header wrapped inside &lt;header> tags. 
 
--- bb-template-as-header.php
+++ bb-template-as-header.php
@@ -6,9 +6,35 @@ Description: Lets you select a template that you've saved in BB to use as a head
 Author: Jatacid
 Version: 1.0.5
 Author URI: http://www.wpbeaverbuilder.com
-GitHub Plugin URI: https://github.com/jatacid/bb-template-as-header
-GitHub Branch:     master
+
 */
+
+include_once('updater.php');
+
+
+if (is_admin()) { // note the use of is_admin() to double check that this is happening in the admin
+    $config = array(
+        'slug' => plugin_basename(__FILE__), // this is the slug of your plugin
+        'proper_folder_name' => 'bb-template-as-header', // this is the name of the folder your plugin lives in
+        'api_url' => 'https://api.github.com/repos/jatacid/bb-template-as-header', // the github API url of your github repo
+        'raw_url' => 'https://raw.github.com/jatacid/bb-template-as-header/master', // the github raw url of your github repo
+        'github_url' => 'https://github.com/jatacid/bb-template-as-header', // the github url of your github repo
+        'zip_url' => 'https://github.com/jatacid/bb-template-as-header/zipball/master', // the zip url of the github repo
+        'sslverify' => true // wether WP should check the validity of the SSL cert when getting an update, see https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/2 and https://github.com/jkudish/WordPress-GitHub-Plugin-Updater/issues/4 for details
+        'requires' => '3.8', // which version of WordPress does your plugin require?
+        'tested' => '4.0', // which version of WordPress is your plugin tested up to?
+        'readme' => 'README.MD' // which file to use as the readme for the version number
+    );
+    new WPGitHubUpdater($config);
+}
+
+
+
+
+
+
+
+
 
 
 require_once dirname( __FILE__ ) . '/insert-pages.php';
