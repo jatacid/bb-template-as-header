1ab050654fe3d093c4659dc7b6a75f5fb93864ba
Jay <jatacid@gmail.com>
2015-11-08 14:55:01 +1000

    updates

--- bb-template-as-header.php
+++ bb-template-as-header.php
@@ -4,17 +4,48 @@ Plugin Name: BB Template as Header
 Plugin URI: http://www.wpbeaverbuilder.com
 Description: Lets you select a template that you've saved in BB to use as a header across every page of your website.
 Author: Jatacid
-Version: 1.1
+Version: 1.0
 Author URI: http://www.wpbeaverbuilder.com
 
 */
 
 
 
-require_once( 'update.php' );
-if ( is_admin() ) {
-    new BFIGitHubPluginUpdater( __FILE__, 'jatacid', "bb-template-as-header" );
+add_action( 'init', 'github_plugin_updater_test_init' );
+function github_plugin_updater_test_init() {
+
+	include_once 'updater.php';
+
+	define( 'WP_GITHUB_FORCE_UPDATE', true );
+
+	if ( is_admin() ) { // note the use of is_admin() to double check that this is happening in the admin
+$login = 'jatacid/bb-template-as-header';
+
+		$config = array(
+			'slug' => plugin_basename( __FILE__ ),
+			'proper_folder_name' => 'github-updater',
+			'api_url' => 'https://api.github.com/repos/' . $login,
+			'raw_url' => 'https://raw.github.com/' . $login .'/master',
+			'github_url' => 'https://github.com/'. $login,
+			'zip_url' => 'https://github.com/'. $login .'/archive/master.zip',
+			'sslverify' => true,
+			'requires' => '3.0',
+			'tested' => '3.3',
+			'readme' => 'README.md',
+			'access_token' => '',
+		);
+
+		new WP_GitHub_Updater( $config );
+
+	}
+
 }
+
+
+
+
+
+
 
 
 
